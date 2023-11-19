c75087b2d8ed0c90925654e6f0649c72f6dfd36e
gitone912 <akash.101@kvsroranchi.in>
2023-11-19 22:44:59 +0530

    time_series

---
+++ core/ai_models/isolation_f.py
@@ -0,0 +1,5 @@
+from sklearn.ensemble import IsolationForest
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import StandardScaler
+
